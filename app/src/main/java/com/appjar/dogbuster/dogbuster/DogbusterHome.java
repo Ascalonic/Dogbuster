35c09bbe3f3355056ff2196c0976817f4bbba936
Arun Baby <arunnellickamuriyil@gmail.com>
2017-06-22 21:38:33 +0530

    upadated by arun

--- app/src/main/java/com/appjar/dogbuster/dogbuster/DogbusterHome.java
+++ app/src/main/java/com/appjar/dogbuster/dogbuster/DogbusterHome.java
@@ -1,13 +1,22 @@
 package com.appjar.dogbuster.dogbuster;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class DogbusterHome extends AppCompatActivity {
+
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +35,25 @@ public class DogbusterHome extends AppCompatActivity {
         });
     }
 
+    private void requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
 }
